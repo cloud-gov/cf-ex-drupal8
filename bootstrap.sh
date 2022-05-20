488e33fbbb28af32063c65f8f3c58fcfa3862872
Mark Boyd <mark.boyd@gsa.gov>
2022-05-20 13:37:05 -0400

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -32,18 +32,17 @@ install_drupal() {
     : "${ROOT_USER_PASS:?Need and root user pass for Drupal}"
 
     drupal site:install \
-        --root="$DOC_ROOT" \
         --no-interaction \
         --account-name="$ROOT_USER_NAME" \
         --account-pass="$ROOT_USER_PASS" \
         --langcode="en"
     # Delete some data created in the "standard" install profile
     # See https://www.drupal.org/project/drupal/issues/2583113
-    drupal --root="$DOC_ROOT" entity:delete shortcut_set default --no-interaction
-    drupal --root="$DOC_ROOT" config:delete active field.field.node.article.body --no-interaction
+    drupal entity:delete shortcut_set default --no-interaction
+    drupal config:delete active field.field.node.article.body --no-interaction
     # Set site uuid to match our config
     UUID=$(grep uuid "$DOC_ROOT"/sites/default/config/system.site.yml | cut -d' ' -f2)
-    drupal --root="$DOC_ROOT" config:override system.site --key uuid --value "$UUID"
+    drupal config:override system.site --key uuid --value "$UUID"
 }
 
 if [ "${CF_INSTANCE_INDEX:-''}" == "0" ] && [ "${APP_NAME}" == "web" ]; then
@@ -52,22 +51,25 @@ if [ "${CF_INSTANCE_INDEX:-''}" == "0" ] && [ "${APP_NAME}" == "web" ]; then
     echo "create database if not exists $DB_NAME;" | mysql --host="$DB_HOST" --port="$DB_PORT" --user="$DB_USER" --password="$DB_PW" || true
   fi
 
-  drupal --root="$DOC_ROOT" list | grep "config:import" > /dev/null || install_drupal
+  # Go into the Drupal web root directory
+  cd "$DOC_ROOT"
+
+  drupal list | grep "config:import" > /dev/null || install_drupal
   # Mild data migration: fully delete database entries related to these
   # modules. These plugins (and the dependencies) can be removed once they've
   # been uninstalled in all environments
 
   # Sync configs from code
-  drupal --root="$DOC_ROOT" config:import --directory "$DOC_ROOT/sites/default/config"
+  drupal config:import --directory "$DOC_ROOT/sites/default/config"
 
   # Secrets
   ADMIN_EMAIL=$(echo "$SECRETS" | jq -r '.ADMIN_EMAIL')
-  drupal --root="$DOC_ROOT" config:override system.site --key mail --value "$ADMIN_EMAIL" > /dev/null
-  drupal --root="$DOC_ROOT" config:override update.settings --key notification.emails.0 --value "$ADMIN_EMAIL" > /dev/null
+  drupal config:override system.site --key mail --value "$ADMIN_EMAIL" > /dev/null
+  drupal config:override update.settings --key notification.emails.0 --value "$ADMIN_EMAIL" > /dev/null
 
   # Import initial content
-  drush --root="$DOC_ROOT" default-content-deploy:import --folder "$DOC_ROOT/sites/default/content" --no-interaction
+  drush default-content-deploy:import --folder "$DOC_ROOT/sites/default/content" --no-interaction
 
   # Clear the cache
-  drupal --root="$DOC_ROOT" cache:rebuild --no-interaction
+  drupal cache:rebuild --no-interaction
 fi
